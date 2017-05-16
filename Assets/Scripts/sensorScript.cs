3abeac41fff3ec7890ab0cbc1bbbfcf2cb62190c
Sahil Singh <sahilsingh10394@gmail.com>
2017-05-16 23:37:48 +0530

    Added Training Car
    Added Sensors and centralized variable handling

---
+++ Assets/Scripts/sensorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensorScript : MonoBehaviour {
+
+	[SerializeField] private int rayRange = 10;
+	[SerializeField] private Color debugColor;
+	public float objectDistance = 100.0f;
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		Debug.DrawRay(transform.position,fwd*rayRange,debugColor);
+		if(Physics.Raycast(transform.position,fwd,out hit,rayRange)){
+			objectDistance = hit.distance;
+		}
+		else{
+			objectDistance = 100.0f;
+		}
+	}
+}
---
+++ Assets/trainingModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trainingModule : MonoBehaviour {
+
+	[SerializeField] private sensorScript[] distanceSensors;
+	private string masterLine = "";
+	void Awake () {
+		GameObject[] sensors = GameObject.FindGameObjectsWithTag("Sensors");
+		for(int i = 0;i<sensors.Length;i++){
+			distanceSensors[i] = sensors[i].GetComponent<sensorScript>();
+		}
+	}
+
+	void Update () {
+		masterLine = string.Concat(masterLine,distanceSensors[0].objectDistance+","+distanceSensors[1].objectDistance+","+distanceSensors[2].objectDistance+","+distanceSensors[3].objectDistance+","+distanceSensors[4].objectDistance+"\n");
+		Debug.Log(masterLine);
+	}
+}
