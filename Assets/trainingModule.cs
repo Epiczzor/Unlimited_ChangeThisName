66192578309e5be441a89601a45fd9adf8e4c9db
Sahil Singh <sahilsingh10394@gmail.com>
2017-05-17 16:24:05 +0530

    Combination
    Server Connection

--- Assets/trainingModule.cs
+++ Assets/trainingModule.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class trainingModule : MonoBehaviour {
 
+	[SerializeField] private int frameDelay = 30;
+	[SerializeField] private string url = "localhost:5000/reinforce?values=";
 	[SerializeField] private sensorScript[] distanceSensors;
-	private string masterLine = "";
+	private int framesElapsed = 0;
+	private int customFrameElapsed = 0;
 	void Awake () {
 		GameObject[] sensors = GameObject.FindGameObjectsWithTag("Sensors");
 		for(int i = 0;i<sensors.Length;i++){
@@ -13,8 +16,33 @@ public class trainingModule : MonoBehaviour {
 		}
 	}
 
-	void Update () {
-		masterLine = string.Concat(masterLine,distanceSensors[0].objectDistance+","+distanceSensors[1].objectDistance+","+distanceSensors[2].objectDistance+","+distanceSensors[3].objectDistance+","+distanceSensors[4].objectDistance+"\n");
-		Debug.Log(masterLine);
+	void customUpdate(){
+		string masterLine = "";
+		masterLine = string.Concat(masterLine,distanceSensors[0].objectDistance+","+distanceSensors[1].objectDistance+","+distanceSensors[2].objectDistance+","+distanceSensors[3].objectDistance+","+distanceSensors[4].objectDistance);
+		//Debug.Log(masterLine);
+		sendVariables(masterLine);
 	}
+
+	private void sendVariables(string master){
+		string updatedURL = string.Concat(url,master);
+		WWW www = new WWW(updatedURL);
+		StartCoroutine(WaitForRequest(www));
+	}
+	IEnumerator WaitForRequest(WWW www){
+		yield return www;
+		if(www.error == null){
+		} 
+		else {
+			Debug.Log("error: "+www.error);
+		}
+	}
+	void Update () {
+		framesElapsed++;
+		if(framesElapsed%frameDelay == 0){
+			customFrameElapsed++;
+			customUpdate();
+		}
+	}
+
+	
 }
